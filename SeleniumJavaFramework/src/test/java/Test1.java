9469fec4b44c291d01c560b92cf8879b023364c8
prave <prave@DESKTOP-G3KQ5NE.hitronhub.home>
2020-12-11 15:40:06 -0500

    Adding drivers and opening Chrome driver (LinkedIn)

--- SeleniumJavaFramework/src/test/java/Test1.java
+++ SeleniumJavaFramework/src/test/java/Test1.java
@@ -1,12 +1,18 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test1 {
 	
 	public static void main(String args[]) {
 		
-		WebDriver driver = new FirefoxDriver();
+		String projectPath = System.getProperty("user.dir");
+		//System.setProperty("webdriver.gecko.driver", projectPath+"\\Web drivers\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", projectPath+"\\Web drivers\\chromedriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
+		driver.close();
 		
 	}
 
